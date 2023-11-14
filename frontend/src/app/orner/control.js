a3bdf641d0a296cbeaa0b1d704db126031cb9f54
mikeneko1224 <amiba725@gmail.com>
2023-11-14 21:52:02 +0900

    change2

--- frontend/src/app/orner/control.js
+++ frontend/src/app/orner/control.js
@@ -13,6 +13,18 @@ const ClickDateTime = () => {
     var hours = currentDate.getHours()
     var min = currentDate.getMinutes()
     console.log("開始打刻を押しました");
+
+    var URL = 'http://127.0.0.1:8000/api/add_time/' + "3" + '/' + '出勤';
+    fetch(URL,
+        {
+        method: 'GET',
+        mode: 'no-cors',  // CORS モード
+        headers: {
+            'Content-Type': 'application/json',
+            // 他のヘッダーが必要な場合は追加
+        },
+    })
+
     //ボタンをクリックした時点の日時をコンソールに表示
     text += year + "/" + month + "/" + date + " " + hours + ":" + min;
     document.getElementById("text").innerHTML = text;
@@ -32,6 +44,18 @@ const ClickRestsTime = () => {
     var hours = currentDate.getHours()
     var min = currentDate.getMinutes()
     console.log("休憩開始打刻を押しました");
+
+    var URL = 'http://127.0.0.1:8000/api/add_time/' + "3" + '/' + '休憩開始';
+    fetch(URL,
+        {
+        method: 'GET',
+        mode: 'no-cors',  // CORS モード
+        headers: {
+            'Content-Type': 'application/json',
+            // 他のヘッダーが必要な場合は追加
+        },
+    })
+
     //ボタンをクリックした時点の日時をコンソールに表示
     text1 += year + "/" + month + "/" + date + " " + hours + ":" + min;
     document.getElementById("text1").innerHTML = text1;
@@ -51,6 +75,18 @@ const ClickendTime = () => {
     var hours = currentDate.getHours()
     var min = currentDate.getMinutes()
     console.log("退勤打刻を押しました");
+
+    var URL = 'http://127.0.0.1:8000/api/add_time/' + "3" + '/' + '退勤';
+    fetch(URL,
+        {
+        method: 'GET',
+        mode: 'no-cors',  // CORS モード
+        headers: {
+            'Content-Type': 'application/json',
+            // 他のヘッダーが必要な場合は追加
+        },
+    })
+
     //ボタンをクリックした時点の日時をコンソールに表示
     text2 += year + "/" + month + "/" + date + " " + hours + ":" + min;
     document.getElementById("text2").innerHTML = text2;
@@ -70,6 +106,17 @@ const ClickRestfTime = () => {
     var hours = currentDate.getHours()
     var min = currentDate.getMinutes()
     console.log("休憩終了打刻を押しました");
+
+    var URL = 'http://127.0.0.1:8000/api/add_time/' + "3" + '/' + '休憩終了';
+    fetch(URL,
+        {
+        method: 'GET',
+        mode: 'no-cors',  // CORS モード
+        headers: {
+            'Content-Type': 'application/json',
+            // 他のヘッダーが必要な場合は追加
+        },
+    })
     //ボタンをクリックした時点の日時をコンソールに表示
     text3 += year + "/" + month + "/" + date + " " + hours + ":" + min;
     document.getElementById("text3").innerHTML = text3;
